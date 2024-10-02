4f120a63f5b2e37ce381b041b7fb2e1e02feb0a6
federicoverbicaro <federico.verbicaro@icloud.com>
2024-10-02 18:14:10 +0200

    create components tasklist

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
-<script setup>
+<script>
+import TaskList from './components/TaskList.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  }
+}
 
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
+  <div class="bg-gray-100 min-h-screen p-4">
+    <div class="text-center mb-10">
+      <h1 class="text-3xl">To Do List Parsonale</h1>
+    </div>
+    <div class="bg-white w-96 p-4 rounded h-auto">
+      <h1 class="text-xl font-bold text-blue-500">Lista delle cose da fare</h1>
+      <div>
+        <TaskList />
+      </div>
+    </div>
+  </div>
+
+
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index" class="bg-white shadow p-2 my-2 rounded border border-gray-400">
+          {{ todo }}
+        </li>
+      </ul>
+      <div class="flex gap-4 items-end">
+        <input v-model="newTodo" type="text" placeholder="Aggiungi una nuova attività" class="p-2 border rounded w-80" />
+        <button @click="addTodo" class="bg-blue-500 text-white px-2 h-10 rounded ">Aggiungi</button>
+      </div>
+     
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        todos: [],
+        newTodo: ''
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo !== '') {
+          this.todos.push(this.newTodo);
+          this.newTodo = '';
+        }
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Stili specifici per il componente TodoList */
+  </style>
+  
