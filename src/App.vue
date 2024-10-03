36789755dd382c36a00bdc4c14019fe8815ad183
federicoverbicaro <federico.verbicaro@icloud.com>
2024-10-03 11:21:37 +0200

    delete columns storage

--- src/App.vue
+++ src/App.vue
@@ -14,50 +14,57 @@ export default {
     }
   },
 
-  mounted(){
-    const storedColumns = localcStorage.getItem('columns')
-    if(storedColumns){
+  mounted() {
+    const storedColumns = localStorage.getItem('columns')
+    if (storedColumns) {
       this.columns = JSON.parse(storedColumns)
-    }
+    };
+
+
   },
 
-  methods:{
-    addTask(index, task){
-      if(task){
-        this.columns[index].task.push(task)
-        this.saveToLocalStorage;
+  methods: {
+    addTask(index, task) {
+      if (task) {
+        this.columns[index].tasks.push(task)
+        this.saveToLocalStorage();
       }
     },
-    addColumn(){
-      const newColumnTitle = prompt ('INserisci il nome della nuova colonna')
-      if(newColumnTitle){
+    removeColumn(index) {
+      this.columns.splice(index, 1) 
+      this.saveToLocalStorage()   
+    },
+
+    saveToLocalStorage() {
+      localStorage.setItem('columns', JSON.stringify(this.columns))
+    },
+
+    addColumn() {
+      const newColumnTitle = prompt('Inserisci il nome della nuova colonna')
+      if (newColumnTitle) {
         this.columns.push({
-        title:newColumnTitle, tasks: []
+          title: newColumnTitle, tasks: []
         })
+        this.saveToLocalStorage();
       }
     }
-
-
   },
-
-  saveToLocalStorage(){
-    localcStorage.setItem('columns', JSON.stringify(this.columns))
-  }
-
 }
 
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen p-4">
+  <div class="bg-gray-100 min-h-screen p-4 overflow-y-scroll ">
     <div class="text-center mb-10">
-      <h1 class="text-3xl">To Do List Parsonale</h1>
-    </div>
-
-    <div class="flex justify-between mt-4">
-      <Column v-for="(column, index) in columns" :key="index" :title="column.title" :tasks="column.tasks" @add-task="addTask(index, $event)"/>
+      <h1 class="text-4xl text-green-500 uppercase">To Do List Parsonale</h1>
     </div>
     <button @click="addColumn" class="mt-4 bg-green-500 text-white p-2 rounded">Aggiungi Colonna</button>
+
+    <div class="flex  gap-5 mt-4">
+      <Column v-for="(column, index) in columns" :key="index" :title="column.title" :tasks="column.tasks"
+        @add-task="addTask(index, $event)" @remove-column="removeColumn(index)" />
+    </div>
+
   </div>
 
 
--- src/components/Column.vue
+++ src/components/Column.vue
@@ -1,35 +1,48 @@
 <template>
     <div class="bg-white shadow p-4 rounded w-1/3">
-        <h2 class="text-xl font-semibold text-center">{{ title }}</h2>
+        <div class="flex  justify-between">
+            <h2 class="text-xl font-semibold capitalize text-blue-500">{{ title }}</h2>
+
+            <button @click="removeColumn" class="bg-red-500 rounded p-2 text-white">Elimina Colonna</button>
+        </div>
+
         <ul class="mt-2">
-            <li v-for="(task,index) in tasks" :key="index" class="bg-gray-200 p-2 my-1 rounded">
+            <li v-for="(task, index) in tasks" :key="index" class="bg-gray-200 p-2 my-1 rounded">
                 {{ task }}
             </li>
         </ul>
-        <input v-model="newTask" type="text" placeholder="Aggiungi un'attività" class="p-2 border rounded my-2"/>
-        <button @click="addTask" class="bg-blue-500 text-white p-2 rounded">Aggiungi</button>
+        <div class="flex justify-between gap-3">
+            <input v-model="newTask" type="text" placeholder="Aggiungi un'attività" class="p-2 border rounded" />
+            <button @click="addTask" class="bg-blue-500 text-white p-2 rounded h-10">Aggiungi</button>
+        </div>
+
     </div>
 </template>
 <script>
 export default {
-    props:{
+    props: {
         title: String,
         tasks: Array
     },
 
-    data(){
-        return{
+    data() {
+        return {
             newTask: ''
         }
     },
 
     methods: {
-        addTask(){
-            if (this.newTask.trim()){
-                this.$emit('add-task',this.newTask)
+        addTask() {
+            if (this.newTask.trim()) {
+                this.$emit('add-task', this.newTask)
                 this.newTask = ''
             }
+        },
+
+        removeColumn() {
+            this.$emit('remove-column') // Emetti l'evento 'remove-column' per il genitore
         }
+
     }
 }
 </script>
